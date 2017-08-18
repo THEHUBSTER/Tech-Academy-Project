1028651c82713d681508d238e1cea899814e187e
THEHUBSTER <darknight.danz@yahoo.com>
2017-08-17 19:12:22 -0700

    Challenge Simple Calculator

---
+++ Simple Calculator/Simple Calculator/Danzcalculator.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Danzcalculator.aspx.cs" Inherits="Simple_Calculator.Danzcalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+        }
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="auto-style1">
+    
+        <strong>Simple Calculator</strong></div>
+        <p>
+            <span class="auto-style2">First Value</span>&nbsp;
+            <asp:TextBox ID="Value1TextBox" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            <span class="auto-style2">Second Value</span>&nbsp;
+            <asp:TextBox ID="Value2TextBox" runat="server" OnTextChanged="Value2TextBox_TextChanged"></asp:TextBox>
+        </p>
+        <asp:Button ID="AddButton" runat="server" OnClick="AddButton_Click" Text="+" />
+&nbsp;
+        <asp:Button ID="SubtractButton" runat="server" OnClick="SubtractButton_Click" Text="-" />
+&nbsp;
+        <asp:Button ID="MultiplicationButton" runat="server" OnClick="MultiplicationButton_Click" Text="*" />
+&nbsp;
+        <asp:Button ID="DivisionButton" runat="server" OnClick="DivisionButton_Click" Text="/" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server" style="font-weight: 700; background-color: #00FFFF"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Simple Calculator/Simple Calculator/Danzcalculator.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Simple_Calculator
+{
+    public partial class Danzcalculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+       {
+          
+        }
+        
+protected void Value2TextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void AddButton_Click(object sender, EventArgs e)
+        {
+            string add = Value1TextBox.Text;
+            int a = int.Parse(add);
+            string add2 = Value2TextBox.Text;
+            int a2 = int.Parse(add2);
+
+            int d = a + a2;
+            resultLabel.Text = d.ToString();
+
+        }
+
+        protected void SubtractButton_Click(object sender, EventArgs e)
+        {
+            string sub = Value1TextBox.Text;
+            int s = int.Parse(sub);
+            string sub2 = Value2TextBox.Text;
+            int s2 = int.Parse(sub2);
+
+            int f = s - s2;
+            resultLabel.Text = f.ToString();
+
+        }
+
+        protected void DivisionButton_Click(object sender, EventArgs e)
+        {
+            string sub = Value1TextBox.Text;
+            double s = int.Parse(sub);
+            string sub2 = Value2TextBox.Text;
+            double s2 = int.Parse(sub2);
+
+            double f = (double)s / (double)s2;
+            resultLabel.Text = f.ToString();
+        }
+
+        protected void MultiplicationButton_Click(object sender, EventArgs e)
+        {
+            string sub = Value1TextBox.Text;
+            int s = int.Parse(sub);
+            string sub2 = Value2TextBox.Text;
+            int s2 = int.Parse(sub2);
+
+            int f = s * s2;
+            resultLabel.Text = f.ToString();
+        }
+    }
+}
